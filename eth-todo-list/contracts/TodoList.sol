c9c81e5b62a0b71e5d5bb3c6022024f0ef222548
Abhinav Robinson <32090909+AbhinavRobinson@users.noreply.github.com>
2019-09-23 22:42:43 +0530

    server init

--- eth-todo-list/contracts/TodoList.sol
+++ eth-todo-list/contracts/TodoList.sol
@@ -1,5 +1,28 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
+    // State of smart contract
     uint public taskCount = 0;
+
+    // About Todo List
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    // Like a dictionary in python
+    mapping(uint => Task) public tasks;
+
+    // Constructor
+    constructor() public{
+        createTask("Check out this Solidity program!");
+    }
+
+    // Assign Task
+    function createTask(string memory _content) public {
+        taskCount ++;
+        // Intialization
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
